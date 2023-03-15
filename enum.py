84978f825319ba7298e6099f48bc42454700ac95
Murf-y <charbelfayad64@gmail.com>
2023-03-15 08:51:38 +0200

    Initial commit :fire:

---
+++ enum.py
@@ -0,0 +1,20 @@
+"""
+Create a Python script that can be run on a website to
+discover subdomains (e.g. admin-test.domain.com), directories (e.g.
+domain.com/for_admins_only/), and files (e.g. domain.com/user_passwords.txt). This
+script will reveal hidden links that may lead to pages with vulnerabilities. Additionally,
+two text files are provided, which you will use to read potential subdomains and
+directories rather than generating them from scratch.
+Your script should be executed from the terminal with the command "python
+test_script.py" and should take an argument that represents the target website's URL.
+The script must store the input file values and write the output to the following files:
+● subdomains_output.bat
+● directories_output.bat
+● files_output.bat
+"""
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
