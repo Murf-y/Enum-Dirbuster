92e3768b0b6f8b506cb460eb9c079557db18b0f1
Murf-y <charbelfayad64@gmail.com>
2023-03-15 08:53:12 +0200

    Add command line argument :rocket:

--- enum.py
+++ enum.py
@@ -13,8 +13,45 @@ The script must store the input file values and write the output to the followin
 ● files_output.bat
 """
 
+
+import sys
+
 def main():
-    pass
+    print("""\n\n                                                                                                  
+EEEEEEEEEEEEEEEEEEEEEENNNNNNNN        NNNNNNNNUUUUUUUU     UUUUUUUUMMMMMMMM               MMMMMMMM
+E::::::::::::::::::::EN:::::::N       N::::::NU::::::U     U::::::UM:::::::M             M:::::::M
+E::::::::::::::::::::EN::::::::N      N::::::NU::::::U     U::::::UM::::::::M           M::::::::M
+EE::::::EEEEEEEEE::::EN:::::::::N     N::::::NUU:::::U     U:::::UUM:::::::::M         M:::::::::M
+  E:::::E       EEEEEEN::::::::::N    N::::::N U:::::U     U:::::U M::::::::::M       M::::::::::M
+  E:::::E             N:::::::::::N   N::::::N U:::::D     D:::::U M:::::::::::M     M:::::::::::M
+  E::::::EEEEEEEEEE   N:::::::N::::N  N::::::N U:::::D     D:::::U M:::::::M::::M   M::::M:::::::M
+  E:::::::::::::::E   N::::::N N::::N N::::::N U:::::D     D:::::U M::::::M M::::M M::::M M::::::M
+  E:::::::::::::::E   N::::::N  N::::N:::::::N U:::::D     D:::::U M::::::M  M::::M::::M  M::::::M
+  E::::::EEEEEEEEEE   N::::::N   N:::::::::::N U:::::D     D:::::U M::::::M   M:::::::M   M::::::M
+  E:::::E             N::::::N    N::::::::::N U:::::D     D:::::U M::::::M    M:::::M    M::::::M
+  E:::::E       EEEEEEN::::::N     N:::::::::N U::::::U   U::::::U M::::::M     MMMMM     M::::::M
+EE::::::EEEEEEEE:::::EN::::::N      N::::::::N U:::::::UUU:::::::U M::::::M               M::::::M
+E::::::::::::::::::::EN::::::N       N:::::::N  UU:::::::::::::UU  M::::::M               M::::::M
+E::::::::::::::::::::EN::::::N        N::::::N    UU:::::::::UU    M::::::M               M::::::M
+EEEEEEEEEEEEEEEEEEEEEENNNNNNNN         NNNNNNN      UUUUUUUUU      MMMMMMMM               MMMMMMMM\n\n""")
+
+
+    print("\n\nThis script can be used for malicious purposes. Use it at your own risk.\n\n")
+
+
+    # Get the target website's URL from the user, passed as an argument
+    args = sys.argv[1:] # Get the arguments from the command line
+
+    if len(args) != 1: # Check if the user passed the correct number of arguments
+
+        print("Error !!!")
+        print("Usage: python enumerate.py <target_website_url>")
+        sys.exit(1)
+    
+    target_url = args[0] # Get the target website's URL from the arguments
+
+    print(f"Target website's URL: {target_url}")
+
 
 if __name__ == "__main__":
     main()
